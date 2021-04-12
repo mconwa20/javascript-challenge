a6737e9a8765d7e340351153abe635eb7f4a0338
Meghan Conway <73912716+mconwa20@users.noreply.github.com>
2021-04-11 20:30:51 -0400

    changing input table data

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,7 @@
 // (see 03-Evr_D3_Table)
 
 // INPUTTING TABLE DATA
-
 var tableData = data;
-console.log(tableData);
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
@@ -11,12 +9,12 @@ var tbody = d3.select("tbody");
 // Console.log the ufo data from data.js
 
 // Loop Through `data` and console.log each ufo report object
-tableData.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
+data.forEach(element => {
+    console.log(element);
     // Use d3 to append one table row 'tr' for each ufo report object
     var row = tbody.append("tr");
     // Use 'Object.entries' to console.log each ufo report value
-    Object.entries(ufoSighting).forEach(function([key, value]) {
+    Object.entries(element).forEach(function([key, value]) {
         console.log(key, value);
         //Append a cell to the row for each value
         var cell = row.append("td");
