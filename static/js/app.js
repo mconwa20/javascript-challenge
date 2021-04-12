c4b9f6eaf8e4ce6d8fa15893f510c5c3f767644e
Meghan Conway <73912716+mconwa20@users.noreply.github.com>
2021-04-11 21:03:06 -0400

    changes

--- static/js/app.js
+++ static/js/app.js
@@ -10,16 +10,16 @@ var tbody = d3.select("tbody");
 
 // Loop Through `data` and console.log each ufo report object
 data.forEach(element => {
-    console.log(element);
+    // console.log(element);
     // Use d3 to append one table row 'tr' for each ufo report object
     var row = tbody.append("tr");
     // Use 'Object.entries' to console.log each ufo report value
-    Object.entries(element).forEach(function([key, value]) {
-        console.log(key, value);
-        //Append a cell to the row for each value
-        var cell = row.append("td");
-        //Use d3 to update each cell's text with ufo report values
-        cell.text(value);
+    Object.entries(element).forEach(([key, value]) => {
+    console.log(key, value);
+    //Append a cell to the row for each value
+    var cell = row.append("td");
+    //Use d3 to update each cell's text with ufo report values
+    cell.text(value);
     });
 });
 
