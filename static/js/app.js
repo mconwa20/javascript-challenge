327808f64800f9fecfeb31bcb41bd056bb69ab76
Meghan Conway <73912716+mconwa20@users.noreply.github.com>
2021-04-12 18:22:23 -0400

    app.js

--- static/js/app.js
+++ static/js/app.js
@@ -6,11 +6,9 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the ufo data from data.js
-
 // Loop Through `data` and console.log each ufo report object
 data.forEach(element => {
-    // console.log(element);
+    console.log(element);
     // Use d3 to append one table row 'tr' for each ufo report object
     var row = tbody.append("tr");
     // Use 'Object.entries' to console.log each ufo report value
@@ -35,13 +33,14 @@ button.on("click", runEnter);
 form.on("submit", runEnter);
 
 function runEnter() {
-
+    // Prevent page from refreshing
     d3.event.preventDefault();
 
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+    var inputElement = d3.select("#form-control");
     var inputValue = inputElement.property("value");
     console.log(inputValue);
+    console.log(tableData)
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
     console.log(filteredData);
 
