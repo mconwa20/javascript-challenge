91a452e39193030af80abefb96ab3df5f7e360bc
Meghan Conway <73912716+mconwa20@users.noreply.github.com>
2021-04-11 20:11:07 -0400

    small changes to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,7 @@
 // INPUTTING TABLE DATA
 
 var tableData = data;
+console.log(tableData);
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
@@ -10,7 +11,8 @@ var tbody = d3.select("tbody");
 // Console.log the ufo data from data.js
 
 // Loop Through `data` and console.log each ufo report object
-tableData.forEach((ufoSighting) => {
+tableData.forEach(ufoSighting) => {
+    console.log(ufoSighting);
     // Use d3 to append one table row 'tr' for each ufo report object
     var row = tbody.append("tr");
     // Use 'Object.entries' to console.log each ufo report value
@@ -28,10 +30,10 @@ tableData.forEach((ufoSighting) => {
 // BUTTON
 
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // Create event handlers for clicking the button or pressing the enter key
-button.on("click", function());
+button.on("click", function() {
 
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
